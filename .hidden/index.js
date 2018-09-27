79b1899314686dcb25f6a86be65ad749ac888920
hat <jburns@chapman.edu>
2018-09-27 16:33:59 -0700

    Added front-end injection

--- .hidden/index.js
+++ .hidden/index.js
@@ -1,1 +1,10 @@
 console.log("Scripts loaded");
+
+// Inject UI
+
+// Needs to check each script tag because I can't a tag throught the Webpack injector
+$('script').each(function () {
+   if ($(this).attr('src') == 'script.js') {
+       $( "<p>Test</p>" ).insertAfter(this);
+   }
+});
