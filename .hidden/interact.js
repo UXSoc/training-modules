b014cd8614795a6e54f50e1237f1a8c08c8b0c35
hat <jburns@chapman.edu>
2018-09-27 15:51:30 -0700

    changed license

--- .hidden/interact.js
+++ .hidden/interact.js
@@ -3,5 +3,9 @@ var colors = require('colors');
 module.exports = {
     run: function(app) {
         console.log("Starting Front End UI".bold);
+        app.get('/test', function(req, res) {
+=            res.json({ custom: 'response' });
+        });
     }
+
 };
