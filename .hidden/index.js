f1015b41f0256534357cac98be8904422cd97b16
jhburns <jburns@chapman.edu>
2018-09-27 21:14:46 -0700

    changed colors

--- .hidden/index.js
+++ .hidden/index.js
@@ -8,7 +8,7 @@ $('script').each(function (index, current) {
        $.get( "./.hidden/public/ui.html", function(data) {
            $(data).insertAfter(current);
 
-           // See colors at https://coolors.co/fcdde0-d7e3eb-f1ffeb-dcfff1-ffe6d8
+           // See colors at https://coolors.co/ffafb6-ffcf98-e1eaff-ffffa2-b2ffa2
            $.get( "/color", function(color_data) {
                $('#injected').css('background-color', color_data.color);
                $('#emoji').text(color_data.emoji);
