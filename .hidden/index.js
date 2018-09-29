5be470c31265629355e448cf2d0eddd59f82f3f0
hat <jburns@chapman.edu>
2018-09-29 14:05:37 -0700

    Added current data serving

--- .hidden/index.js
+++ .hidden/index.js
@@ -14,13 +14,16 @@ function after_inject() {
     click_call('#next', '/next');
     click_call('#back', '/back');
     click_call('#wipe', '/wipe');
-    click_call('#home', '/go_home');
+    
+    $.get('/current', function (data) {
+        console.log(data);
+    });
 }
 
 // Needs to check each script tag because I can't a tag throught the Webpack injector
 $('script').each(function (index, current) {
    if ($(current).attr('src') == 'script.js') {
-       $.get( "./.hidden/public/ui.html", function(data) {
+       $.get('./.hidden/public/ui.html', function(data) {
            $(current).before("<!-- This html is injected when serving the page to make the UI -->");
            $(current).before("<!-- You can safely ignore everything past here -->");
            $(data).insertAfter(current);
@@ -28,10 +31,10 @@ $('script').each(function (index, current) {
            after_inject();
 
            // See colors at https://coolors.co/ffafb6-ffcf98-e1eaff-ffffa2-b2ffa2
-           $.get( "/on-load", function(color_data) {
+           $.get('/on-load', function(color_data) {
                $('#injected').css('background-color', color_data.color);
                $('#emoji').text(color_data.emoji);
-           })
+           });
       });
    }
 });
--- .hidden/interact.js
+++ .hidden/interact.js
@@ -1,13 +1,23 @@
 var colors = require('colors');
-var config = require('./config.json');
+var options = require('./config.json');
 var emoji_engine = require('node-emoji');
 
 var uniqueRandomArray = require('unique-random-array');
-var rand_color = uniqueRandomArray(config.colors);
-var rand_emoji = uniqueRandomArray(config.emojis);
+var rand_color = uniqueRandomArray(options.colors);
+var rand_emoji = uniqueRandomArray(options.emojis);
+
+//return - string of folder name
+function get_current_folder() {
+    var current_folder = options.modules[options.current - 1].folder;
+    return current_folder;
+}
+
+function get_current_json() {
+    return require('./modules/' + get_current_folder() + '/template.json');
+}
 
 function special_requests(app) {
-  app.get('/on-load', function(req, res) {
+    app.get('/on-load', function(req, res) {
     var color = rand_color();
     var emoji = emoji_engine.get(rand_emoji());
 
@@ -16,9 +26,19 @@ function special_requests(app) {
        emoji
      };
      res.json(output);
-  });
+    });
 
+    app.get('/current', function (req, res) {
+        var current_config = get_current_json();
 
+        const send_data = {
+            title: current_config.title,
+            number: options.current,
+        };
+
+        res.json(send_data);
+        console.log("Sending module " + current_config.title.green.bold + ' #' + options.current + " successfully");
+    });
 }
 
 module.exports = {
