3996bfbf065209dca2a353a9843a54dcd831a671
hat <jburns@chapman.edu>
2018-09-30 02:40:29 -0700

    Final Version part two

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-const HtmlWebpackInsertAtBodyEndPlugin = require('html-webpack-insert-at-body-end');
 
 module.exports = {
     mode: 'development',
